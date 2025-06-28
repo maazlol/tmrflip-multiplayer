898ad557d32c079a8993ac6fb660147e78bafc2b
maazlol <93197112+maazlol@users.noreply.github.com>
2025-06-28 06:14:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,44 +13,61 @@ let games = {};
 io.on('connection', (socket) => {
   socket.on('join-game', ({ name, code }) => {
     if (!games[code]) {
-      games[code] = [];
+      games[code] = { players: [], started: false };
     }
 
-    if (games[code].includes(name)) {
+    if (games[code].players.includes(name)) {
       socket.emit('name-taken');
       return;
     }
 
-    games[code].push(name);
+    games[code].players.push(name);
     socket.join(code);
     socket.code = code;
     socket.name = name;
 
-    io.to(code).emit('player-list', games[code]);
+    io.to(code).emit('player-list', games[code].players);
+  });
 
-    socket.on('chat-message', ({ name, message }) => {
-      io.to(code).emit('chat-message', { name, message });
-    });
+  socket.on('start-game', ({ code }) => {
+    const game = games[code];
+    if (!game || game.started) return;
 
-    socket.on('start-game', ({ code }) => {
-      io.to(code).emit('start-game');
-    });
+    game.started = true;
 
-    socket.on('disconnect', () => {
-      const code = socket.code;
-      const name = socket.name;
+    const hands = {};
+    for (const player of game.players) {
+      hands[player] = drawCards(3); // basic 3 cards per player
+    }
 
-      if (games[code]) {
-        games[code] = games[code].filter(n => n !== name);
-        io.to(code).emit('player-list', games[code]);
+    io.to(code).emit('deal-hand', hands);
+  });
 
-        if (games[code].length === 0) {
-          delete games[code];
-        }
-      }
-    });
+  socket.on('chat-message', ({ name, message }) => {
+    const code = socket.code;
+    io.to(code).emit('chat-message', { name, message });
+  });
+
+  socket.on('disconnect', () => {
+    const code = socket.code;
+    const name = socket.name;
+
+    if (games[code]) {
+      games[code].players = games[code].players.filter(n => n !== name);
+      io.to(code).emit('player-list', games[code].players);
+      if (games[code].players.length === 0) delete games[code];
+    }
   });
 });
+
+function drawCards(count) {
+  const pool = ['🔴 1', '🟡 2', '🔵 Skip', '🟢 +2', '🔴 9', '🟡 5', '🟢 Reverse', '🔵 +4'];
+  const cards = [];
+  for (let i = 0; i < count; i++) {
+    cards.push(pool[Math.floor(Math.random() * pool.length)]);
+  }
+  return cards;
+}
 
 http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
