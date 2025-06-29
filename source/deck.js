c9d98f2d032c0f0048ea915eb9df8a564dd6b00f
maazlol <93197112+maazlol@users.noreply.github.com>
2025-06-29 19:11:05 +0500

    deck.js

--- source/deck.js
+++ source/deck.js
@@ -1,17 +1,34 @@
-const colors = ["Red", "Green", "Blue", "Yellow"];
-const values = ["0", "1", ..., "Reverse", "+2", "Skip", "Wild", "Wild+4"];
+const Card = require("./card");
 
 function generateDeck() {
-  let deck = [];
+  const colors = ["Red", "Green", "Blue", "Yellow"];
+  const values = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "Skip", "Reverse", "+2"
+  ];
+  const deck = [];
+
   for (let color of colors) {
     for (let value of values) {
-      if (value.startsWith("Wild")) {
-        deck.push({ color: "Wild", value });
-      } else {
-        deck.push({ color, value });
-      }
+      deck.push(new Card(color, value));
+      if (value !== "0") deck.push(new Card(color, value)); // two of each except 0
     }
   }
-  return deck;
+
+  for (let i = 0; i < 4; i++) {
+    deck.push(new Card("Wild", "Wild"));
+    deck.push(new Card("Wild", "+4"));
+  }
+
+  return shuffle(deck);
 }
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 module.exports = { generateDeck };
